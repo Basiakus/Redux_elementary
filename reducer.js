95bac20f0274ba94ad73ea6f62dd234953c701b3
Adam Basiak <basiakus@gmail.com>
2018-01-11 21:12:25 +0100

    jeja

--- reducer.js
+++ reducer.js
@@ -14,16 +14,24 @@ function resucer(state, action) {
 function rediucer(state = inicialState, action) {
 	switch(action.type) {
 		case ADD_COMMENT:
-		return Object.assign({}, state, {
-			comments: [
-				{
-					id: action.id,
-					text: action.text,
-					votes: 0
-				},
-				...state
-			]
-		})
+			return Object.assign({}, state, {
+				comments: [
+					{
+						id: action.id,
+						text: action.text,
+						votes: 0
+					},
+					...state
+				]
+			});
+		case REMOVE_COMMENT:
+			return Object.assign({}, state, {
+				comments: state.comments.filter(comment => comment.id !== action.id)
+			});
+		case EDIT_COMMENT:
+			return Object.assign({}, state, {
+				comments: state.comments.find()
+			}); 
 	default:
 		return state;
 	}
