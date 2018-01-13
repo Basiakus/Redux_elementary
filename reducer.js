141af4ca4d06b7b5847616ea7e1d5db89b5da78f
Adam Basiak <basiakus@gmail.com>
2018-01-13 15:34:23 +0100

    delete app runction

--- reducer.js
+++ reducer.js
@@ -11,12 +11,6 @@ const inicialState = {
     users: []
 };
 
-function app(state = initialState, action) {
-    return {
-        comments: comments(state.comments, action),
-        users: users(state.users, action)
-    };
-}
 export default reducer;
 
 
