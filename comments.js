713b12b0251451db32e6a1168598354f4f7303ff
Adam Basiak <basiakus@gmail.com>
2018-01-13 15:29:28 +0100

    change brackets position in map function

--- comments.js
+++ comments.js
@@ -22,10 +22,10 @@ function comments(state = [], action) {
 			return state.filter(comment => comment.id !== action.id);
 			
 		case EDIT_COMMENT:
-			return state.map(comment => comment.id === action.id) ? {...comment, text: action.text} : comment;
+			return state.map(comment => comment.id === action.id ? {...comment, text: action.text} : comment);
 		case THUMB_UP_COMMENT:
-			return state.map(comment => comment.id === action.id) ? {...comment, votes: comment.votes + 1} : comment;
+			return state.map(comment => comment.id === action.id ? {...comment, votes: comment.votes + 1} : comment);
 		case THUMB_DOWN_COMMENT:
-			return state.map(comment => comment.id === action.id) ? {...comment, votes: comment.votes - 1} : comment;   
+			return state.map(comment => comment.id === action.id ? {...comment, votes: comment.votes - 1} : comment);   
     } 
 }
